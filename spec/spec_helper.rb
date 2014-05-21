9199d6f952e3cb8470aedf9a0aa5dbb5fbf22d38
Hsiu-Fan Wang <hfwang@porkbuns.net>
2014-05-21 11:17:28 -0700

    Disable codeclimate coverage for 1.8.7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,10 @@ require "ar_protobuf_store"
 
 require "active_record"
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if RUBY_VERSION > "1.8.7"
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 ActiveRecord::Schema.verbose = false
